bbd0bd497acbff93ba3579e28c24232cce3f901b
Danny Libin <dlibinrx@gmail.com>
2023-08-27 16:08:50 -0500

    attempt at jinja clone

--- src/build.py
+++ src/build.py
@@ -10,7 +10,7 @@ from shutil import copy
 from urllib import request
 from typing import Union
 
-from utilities import cprint, CColors
+from utilities import cprint, CColors, render_template
 
 wp_api_base_path = "http://admin.innerpathllc.com/wp-json/wp/v2"
 project_base_path = Path(__file__).parent.parent
@@ -52,15 +52,6 @@ def wp_get_all(url: str, use_cache=False):
             json.dump(wp_res, f)
 
     return wp_res
-
-
-class DropdownChild:
-    name: Union[str, None]
-    href: str
-
-    def __init__(self, name: Union[str, None], href: str):
-        self.name = name
-        self.href = href
 
 
 def menu_item_solo(name: str, href: str) -> str:
@@ -107,7 +98,7 @@ def menu_dropdown(name: str, children: list[DropdownChild]) -> str:
     return parent_template.replace(children_tag, children_template)
 
 
-def get_menu(use_cache=False) -> str:
+def get_menu_old(use_cache=False) -> str:
     menu = wp_get_all("menu", use_cache)
     nav_arr: list[NavItem] = []
 
@@ -135,7 +126,7 @@ def get_menu(use_cache=False) -> str:
             item_url_parts[-2] if item_url_parts[-1] == "" else item_url_parts[-1]
         )
         if menu_item["title"] == "Home":
-            target_slug = "/"
+            target_slug = ""
         nav_arr.append(
             NavItem(
                 menu_item["ID"],
@@ -181,7 +172,81 @@ def get_menu(use_cache=False) -> str:
     return main_menu
 
 
-def build(clean=False, use_wp_cache=False):
+def get_menu(use_cache=False) -> str:
+    menu = wp_get_all("menu", use_cache)
+    nav_arr: list[NavItem] = []
+
+    # Convert raw menu dictionary to typed Python class
+    for menu_item in menu:
+        item_url_parts = menu_item["url"].split("/")
+
+        # Parent items that aren't a link themselves, but have
+        # Sub-items (aka click/hover to expand submenu)
+        if item_url_parts[0] == "":
+            nav_arr.append(
+                NavItem(
+                    menu_item["ID"],
+                    menu_item["title"],
+                    0,
+                    menu_item["menu_order"],
+                    "",
+                    [],
+                )
+            )
+            continue
+
+        # Base menu items or sub-items for a parent menu
+        target_slug = (
+            item_url_parts[-2] if item_url_parts[-1] == "" else item_url_parts[-1]
+        )
+        if menu_item["title"] == "Home":
+            target_slug = "/"
+        nav_arr.append(
+            NavItem(
+                menu_item["ID"],
+                menu_item["title"],
+                int(menu_item["menu_item_parent"]),
+                menu_item["menu_order"],
+                target_slug,
+                [],
+            )
+        )
+
+    # Collect base menu and child items when present for parent menus
+    parent_items = sorted(
+        [item for item in nav_arr if item.parent_wp_id == 0],
+        key=lambda item: item.sort_order,
+    )
+    for parent in parent_items:
+        parent.children = sorted(
+            [child for child in nav_arr if child.parent_wp_id == parent.wp_id],
+            key=lambda item: item.sort_order,
+        )
+
+    # # Generate the HTML string for all the nav links
+    # for parent in parent_items:
+    #     if len(parent.children) > 0:
+    #         parent.children.sort(key=lambda item: item.sort_order)
+    #         children = [
+    #             DropdownChild(name=child.name, href=f"/{child.target_slug}.html")
+    #             for child in parent.children
+    #         ]
+    #         main_menu += menu_dropdown(parent.name, children)
+    #     else:
+    #         if parent.name == "Home":
+    #             main_menu += menu_item_solo(parent.name, "/")
+    #         else:
+    #             main_menu += menu_item_solo(parent.name, f"/{parent.target_slug}.html")
+    #
+    # with project_base_path.joinpath("src/navbar.html").open("r") as f:
+    #     main_menu = f.read().replace("{{main_menu}}", main_menu)
+
+    return render_template(
+        project_base_path.joinpath("src/navbar.html"), main_menu=parent_items
+    )
+
+
+def build_old(clean=False, use_wp_cache=False):
     src = project_base_path / "src"
     build_base_path = project_base_path / "public"
 
@@ -263,6 +328,65 @@ def build(clean=False, use_wp_cache=False):
             "index.html" if is_index else f"{page['slug']}.html",
         ).open("w", encoding="utf-8") as f:
             f.write(page_built)
+
+
+def build(clean=False, use_wp_cache=False):
+    src = project_base_path / "src"
+    build_base_path = project_base_path / "public"
+
+    if clean and build_base_path.exists():
+        build_base_path.unlink()
+
+    makedirs(build_base_path, exist_ok=True)
+
+    # Copy cached static file to public dir only on clean build
+    if clean:
+        for static_filename in listdir(src / "static-cached"):
+            copy(src / "static-cached" / static_filename, build_base_path)
+
+    pages = wp_get_all("pages", use_wp_cache)
+    pdfs = [
+        {
+            "slug": pdf["slug"],
+            "display_string": pdf["title"]["rendered"],
+            "url": pdf["source_url"],
+        }
+        for pdf in wp_get_all("media")
+        if pdf["mime_type"] == "application/pdf"
+    ]
+
+    for pdf in pdfs:
+        pdf_path = build_base_path.joinpath(f"{pdf['slug']}.pdf")
+        # For dev purposes, avoid redownloading every rebuild
+        if not pdf_path.exists():
+            request.urlretrieve(pdf["url"], pdf_path)
+
+    # Copy all static files to the public dir as is
+    for static_filename in listdir(src / "static"):
+        copy(src / "static" / static_filename, build_base_path)
+
+    # For each standard Wordpress page, generate an HTML page using our layout template
+    for page in pages:
+        if not page["status"] == "publish":
+            continue
+
+        title = page["title"]["rendered"] if page["title"]["rendered"] != "Home" else ""
+        is_index = title == "Home"
+
+        template_name = title.lower().replace(" ", "-")
+        if not src.joinpath(f"templates/{template_name}.html").exists():
+            # Pages with article template
+            render_template(
+                src.joinpath(f"templates/article.html"),
+                dest_path=build_base_path.joinpath(
+                    "index.html" if is_index else f"{page['slug']}.html"
+                ),
+                title=title,
+                wordpress_source=page["content"]["rendered"],
+            )
+        else:
+            # Pages with their own template
+            pass
 
 
 if __name__ == "__main__":
